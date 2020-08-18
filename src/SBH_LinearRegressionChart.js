f49526092d07c9e28ad314d0eee4fcf0ad638a72
Daniel <elrion018@gmail.com>
2020-08-18 12:48:50 +0900

    add linear-regression-chart

--- src/SBH_LinearRegressionChart.js
+++ src/SBH_LinearRegressionChart.js
@@ -1,159 +1,148 @@
 // src/SBH_LinearRegressionChart.js
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import ReactResizeDetector from 'react-resize-detector';
+import ReactResizeDetector from "react-resize-detector";
 
-import Highcharts from 'highcharts';
+import Highcharts from "highcharts";
 
-import UUIDv1 from 'uuid/v1';
+import UUIDv1 from "uuid/v1";
 
 const Root = styled.div`
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 `;
 
 class SBH_LinearRegressionChart extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.chart = {};
+    this.chart = {};
 
-        this.state = {
-            chartId: UUIDv1(),
-        };
-    }
+    this.state = {
+      chartId: UUIDv1(),
+    };
+  }
 
-    componentDidMount() {
-        this.chart = Highcharts.chart(this.state.chartId, {
-            title: {
-                text: this.props.title,
+  componentDidMount() {
+    this.chart = Highcharts.chart(this.state.chartId, {
+      title: {
+        text: this.props.title,
+      },
+
+      xAxis: {
+        title: {
+          text: this.props.xAxisDataKey,
+        },
+      },
+
+      yAxis: {
+        title: {
+          text: this.props.yAxisDataKey,
+        },
+      },
+
+      plotOptions: {
+        series: {
+          label: {
+            connectorAllowed: false,
+          },
+        },
+      },
+
+      series: [
+        {
+          type: "line",
+          name: this.props.yAxisDataKey,
+          data: this.jsonArrayToLineData(this.props.data),
+        },
+        {
+          type: "scatter",
+          name: this.props.xAxisDataKey,
+          data: this.jsonArrayToScatterData(this.props.data),
+          marker: {
+            radius: 4,
+          },
+        },
+      ],
+
+      responsive: {
+        rules: [
+          {
+            condition: {
+              maxWidth: 500,
             },
-
-            xAxis: {
+            chartOptions: {
+              legend: {
+                align: "center",
+                verticalAlign: "bottom",
+                layout: "horizontal",
+              },
+              yAxis: {
+                labels: {
+                  align: "left",
+                  x: 0,
+                  y: -5,
+                },
                 title: {
-                    text: this.props.xAxisDataKey,
+                  text: null,
                 },
-            },
-
-            yAxis: {
-                title: {
-                    text: this.props.yAxisDataKey,
-                },
-            },
-
-            plotOptions: {
-                series: {
-                    label: {
-                        connectorAllowed: false,
-                    },
-                },
-            },
-
-            series: [
-                {
-                    type: 'line',
-                    name: this.props.yAxisDataKey,
-                    data: this.jsonArrayToLineData(this.props.data),
-                },
-                {
-                    type: 'scatter',
-                    name: this.props.xAxisDataKey,
-                    data: this.jsonArrayToScatterData(this.props.data),
-                    marker: {
-                        radius: 4,
-                    },
-                },
-            ],
-
-            responsive: {
-                rules: [
-                    {
-                        condition: {
-                            maxWidth: 500,
-                        },
-                        chartOptions: {
-                            legend: {
-                                align: 'center',
-                                verticalAlign: 'bottom',
-                                layout: 'horizontal',
-                            },
-                            yAxis: {
-                                labels: {
-                                    align: 'left',
-                                    x: 0,
-                                    y: -5,
-                                },
-                                title: {
-                                    text: null,
-                                },
-                            },
-                            subtitle: {
-                                text: null,
-                            },
-                            credits: {
-                                enabled: false,
-                            },
-                        },
-                    },
-                ],
-            },
-
-            credits: {
+              },
+              subtitle: {
+                text: null,
+              },
+              credits: {
                 enabled: false,
+              },
             },
-        });
-    }
+          },
+        ],
+      },
 
-    onResize = (width, height) => {
-        this.chart.setSize(width, height, false);
-    };
+      credits: {
+        enabled: false,
+      },
+    });
+  }
 
-    jsonArrayToSeriesData = (name, jsonArray, formatter) => {
-        var seriesData = [];
+  onResize = (width, height) => {
+    this.chart.setSize(width, height, false);
+  };
 
-        jsonArray.map((jsonObject) => {
-            // console.log(jsonObject);
+  jsonArrayToLineData = (jsonArray) => {
+    var regressionlineData = [];
+    jsonArray[0].regressionlineData.map((jsonObject) => {
+      regressionlineData.push([jsonObject.xValue, jsonObject.yValue]);
+    });
+    //RegressionlineData of jsonArray[0] is jsonArray having two json objects with xValue, yValue. (the two points of regression line)
+    //The two points are the first and last points based on the x-axis of the regression line.
 
-            if (formatter && typeof formatter === 'function') {
-                seriesData.push(formatter(jsonObject[name]));
-            } else {
-                seriesData.push(jsonObject[name]);
-            }
-        });
+    return regressionlineData;
+  };
+  jsonArrayToScatterData = (jsonArray) => {
+    var scatterData = [];
+    jsonArray[0].scatterData.map((jsonObject) => {
+      //ScatterData of jsonArray[0] is jsonArray having many json objects with xValue, yValue.
+      //ScatterData of jsonArray[0] is raw data.
+      scatterData.push([jsonObject.xValue, jsonObject.yValue]);
+    });
+    return scatterData;
+  };
 
-        return seriesData;
-    };
-    jsonArrayToLineData = (jsonArray) => {
-        var regressionlineData = [];
-        jsonArray[0].regressionlineData.map((jsonObject) => {
-            regressionlineData.push([jsonObject.xValue, jsonObject.yValue]);
-        });
+  render() {
+    const { chartId } = this.state;
 
-        return regressionlineData;
-    };
-    jsonArrayToScatterData = (jsonArray) => {
-        var scatterData = [];
-        jsonArray[0].scatterData.map((jsonObject) => {
-            scatterData.push([jsonObject.xValue, jsonObject.yValue]);
-        });
-        return scatterData;
-    };
-
-    render() {
-        const { chartId } = this.state;
-
-        return (
-            <Root id={chartId}>
-                <ReactResizeDetector
-                    resizableElementId={chartId}
-                    handleWidth
-                    handleHeight
-                    onResize={this.onResize}
-                />
-            </Root>
-        );
-    }
+    return (
+      <Root id={chartId}>
+        <ReactResizeDetector
+          resizableElementId={chartId}
+          handleWidth
+          handleHeight
+          onResize={this.onResize}
+        />
+      </Root>
+    );
+  }
 }
 
 SBH_LinearRegressionChart.propTypes = {};
